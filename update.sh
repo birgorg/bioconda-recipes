d6e7dce68ed40faa4e519212c4a60ce05f081370
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-05 11:32:50 +0200

    First take on an update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone and copy
+git clone https://github.com/bioconda/bioconda-recipes
+cp -r bioconda-recipes/scripts scripts
+cp bioconda-recipes/config.yml config.yml
+cp bioconda-recipes/bootstrap.py bootstrap.py
+cp bioconda-recipes/build-fail-blacklist build-fail-blacklist
+rm bioconda-recipes
+
+git add scripts/ config.yml bootstrap.py build-fail-blacklist
+git commit -m "Updated: $(data)"
+git push
