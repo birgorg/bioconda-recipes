52f1c200e69ca0f15fbfbc5e01d3fe38534f5ab9
Per Hogfeldt <hogfeldt38@gmail.com>
2020-04-05 11:45:25 +0200

    Added flags for removing bioconda-recipes

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ cp -r bioconda-recipes/scripts scripts
 cp bioconda-recipes/config.yml config.yml
 cp bioconda-recipes/bootstrap.py bootstrap.py
 cp bioconda-recipes/build-fail-blacklist build-fail-blacklist
-rm bioconda-recipes
+rm -rf bioconda-recipes
 
 git add scripts/ config.yml bootstrap.py build-fail-blacklist
 git commit -m "Update script: I have updated this repo $(date)"
